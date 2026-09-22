252771f3cf542895b12003845870202026ee1dca
Dennis Kimaita <dkimaita22@gmail.com>
2026-09-22 17:44:07 +0300

    add validation pass to check for overfitting

--- training_loop.py
+++ training_loop.py
@@ -1,12 +1,13 @@
+import torch
 import torch.optim as optim
 import torch.nn as nn
 from trunk_layer import ChessNet
-from training_model import train_loader
+from training_model import train_loader, val_loader
 
 
 model = ChessNet()
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
+optimizer = optim.Adam(model.parameters(), lr=1e-3) # Adam runs the optimization algorithm with a learning rate of 0.001(1e-3) and updates the model's parameters based on the computed gradients from the loss function
 
 num_epochs = 10
 
@@ -21,7 +22,7 @@ for epoch in range(num_epochs):
         # forward pass — get the three logits from the model
         from_logits, to_logits, promo_logits = model(boards) 
 
-        # compute the three losses using criterion, then sum them into one `loss`
+        # compute the loss 
         loss_from = criterion(from_logits, from_labels)
         loss_to = criterion(to_logits, to_labels)   
         loss_promo = criterion(promo_logits, promo_labels)
@@ -35,3 +36,24 @@ for epoch in range(num_epochs):
 
     avg_loss = total_loss / len(train_loader)
     print(f"Epoch {epoch+1}/{num_epochs} - Train loss: {avg_loss:.4f}")
+
+
+    # --- validation pass ---
+    model.eval()  # evaluate mode — disables dropout and batchnorm layers 
+    val_loss = 0
+
+    with torch.no_grad():
+        for boards, from_labels, to_labels, promo_labels in val_loader:
+            # forward pass
+            from_logits, to_logits, promo_logits = model(boards)
+
+            # compute the loss (exactly the same as in training, but we don't do backward pass or optimizer step)
+            loss_from = criterion(from_logits, from_labels)
+            loss_to = criterion(to_logits, to_labels)
+            loss_promo = criterion(promo_logits, promo_labels)
+            loss = loss_from + loss_to + loss_promo
+
+            val_loss += loss.item()
+
+    avg_val_loss = val_loss / len(val_loader)
+    print(f"Epoch {epoch+1}/{num_epochs} - Val loss: {avg_val_loss:.4f}")
